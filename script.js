a49e960dfa9f264bf06d0a01fb315c3555e1277f
Leandru Martin <leandrumartin@gmail.com>
2026-02-01 15:49:52 -0600

    Add static cookie notice

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', Math.min(0.9999, window.scrollY / window.innerHeight * 2))
   document.body.style.setProperty('--scroll-full', Math.min(window.scrollY))
 }, false)
+
