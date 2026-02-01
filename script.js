c9b66fd340d154a4fb4c8f6e113a26d70ad7cb34
Leandru Martin <leandrumartin@gmail.com>
2026-02-01 15:56:52 -0600

    Make cookie notice appear 10% of the time

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ window.addEventListener('scroll', () => {
   document.body.style.setProperty('--scroll-full', Math.min(window.scrollY))
 }, false)
 
+if (Math.random() < 0.1) {
+    let cookieNotice = document.createElement('div')
+    cookieNotice.id = 'cookie-notice'
+    cookieNotice.innerHTML = `
+      <p>
+        This website does not use cookies.
+      </p>
+      <button id="cookie-notice-dismiss" onclick="document.getElementById('cookie-notice').style.display='none'">OK</button>
+    `
+    document.body.appendChild(cookieNotice)
+}
