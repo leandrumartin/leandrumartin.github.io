d8557b85ba29b5b8d072faf6be75586aea7c61c1
Leandru Martin <leandrumartin@gmail.com>
2026-02-10 13:21:19 -0600

    Create portfolio-header template and begin working on adjusting styles to make it work

--- script.js
+++ script.js
@@ -1,51 +1,28 @@
-import * as data from './portfolio_entries.json' with {type: 'json'};
+import style from './style.css' with {type: 'css'};
+
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = './style.css';
 
 window.addEventListener('scroll', () => {
-  document.body.style.setProperty('--scroll', Math.min(0.9999, window.scrollY / window.innerHeight * 2))
+    document.body.style.setProperty('--scroll', Math.min(0.9999, window.scrollY / window.innerHeight * 2))
 }, false)
 
-const collaborativeProjects = document.getElementById('collaborative-projects');
-const individualProjects = document.getElementById('individual-projects');
+class PortfolioHeader extends HTMLElement {
+    constructor() {
+        super();
+    }
 
-data.default.collaborativeProjects.forEach((project) => {
-  const entryDiv = document.createElement("div");
-  entryDiv.classList.add("portfolio-entry");
+    connectedCallback() {
+        let template = document.getElementById("portfolio-header-template");
+        let templateContent = template.content.cloneNode(true);
 
-  entryDiv.innerHTML = `
-    <div class="portfolio-header-section">
-      <div class="portfolio-entry-item portfolio-header">
-        <h3>
-          ${project.link ?
-    `<a href="${project.link}">${project.title}</a>` :
-    project.title
-  }
-        </h3>
-      </div>
-      ${project.screenshot ?
-    `<div class="portfolio-entry-item portfolio-screenshot">
-          <img src="${project.screenshot}" alt="" />
-        </div>` :
-    ""
-  }
-    </div>
-    <div class="portfolio-description portfolio-entry-item">
-      ${project.description.map(paragraph => `<p>${paragraph}</p>`).join("")}
-      ${project.github ?
-    `<p class="github-link">
-      <a href="${project.github}">View on GitHub</a>
-    </p>` :
-    ""
-  }
-      <ul class="technologies-icons">
-        ${project.technologies
-    .map(
-      tech =>
-        `<li><img src="${tech.icon}" alt="" /><span>${tech.name}</span></li>`
-    )
-    .join("")}
-      </ul>
-    </div>
-  `;
+        const shadowRoot = this.attachShadow({mode: "open"});
+        shadowRoot.appendChild(document.importNode(templateContent, true));
 
-  collaborativeProjects.appendChild(entryDiv);
-})
+        shadowRoot.adoptedStyleSheets = [style];
+        // shadowRoot.appendChild(link);
+    }
+}
+
+customElements.define('portfolio-header', PortfolioHeader);
