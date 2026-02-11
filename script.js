6b486b23bb6af204a66f9f975af73b2209c0d7f6
Leandru Martin <leandrumartin@gmail.com>
2026-02-10 22:57:42 -0600

    Add back JSON approach

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import * as data from './portfolio_entries.json' with {type: 'json'};
 import style from './style.css' with {type: 'css'};
 
 const link = document.createElement('link');
@@ -40,3 +41,49 @@ class PortfolioEntry extends HTMLElement {
 }
 
 customElements.define('portfolio-entry', PortfolioEntry);
+
+const collaborativeProjects = document.getElementById('collaborative-projects');
+const individualProjects = document.getElementById('individual-projects');
+
+data.default.collaborativeProjects.forEach((project) => {
+    const entryDiv = document.createElement("div");
+    entryDiv.classList.add("portfolio-entry");
+
+    entryDiv.innerHTML = `
+    <div class="portfolio-header-section">
+      <div class="portfolio-entry-item portfolio-header">
+        <h3>
+          ${project.link ?
+        `<a href="${project.link}">${project.title}</a>` :
+        project.title
+    }
+        </h3>
+      </div>
+      ${project.screenshot ?
+        `<div class="portfolio-entry-item portfolio-screenshot">
+          <img src="${project.screenshot}" alt="" />
+        </div>` :
+        ""
+    }
+    </div>
+    <div class="portfolio-description portfolio-entry-item">
+      ${project.description.map(paragraph => `<p>${paragraph}</p>`).join("")}
+      ${project.github ?
+        `<p class="github-link">
+      <a href="${project.github}">View on GitHub</a>
+    </p>` :
+        ""
+    }
+      <ul class="technologies-icons">
+        ${project.technologies
+        .map(
+            tech =>
+                `<li><img src="${tech.icon}" alt="" /><span>${tech.name}</span></li>`
+        )
+        .join("")}
+      </ul>
+    </div>
+  `;
+
+    collaborativeProjects.appendChild(entryDiv);
+})
