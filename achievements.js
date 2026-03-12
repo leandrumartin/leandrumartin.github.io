eb916a4b82db4d5a5c6c9c78e37dd290974d1093
Leandru Martin <leandrumartin@gmail.com>
2026-03-12 19:54:52 -0400

    Style pane open/close button

--- achievements.js
+++ achievements.js
@@ -1,12 +1,14 @@
 let open = false
-let achievementsPane = document.querySelector("#achievements-pane");
+let achievementsButton = document.querySelector("#achievements-open-button")
+let achievementsPane = document.querySelector("#achievements-pane")
 
-document.querySelector("#achievements-open-button").addEventListener("click", () => {
+achievementsButton.addEventListener("click", () => {
   open = !open
-  console.log(open)
   if (open) {
+    achievementsButton.style.backgroundColor = "var(--bg-primary-color)"
     achievementsPane.style.transform = "none"
   } else {
+    achievementsButton.style.backgroundColor = "unset"
     achievementsPane.style.transform = "translateX(calc(-100% + var(--button-width) + var(--pane-border) - var(--pane-margin)))"
   }
 })
